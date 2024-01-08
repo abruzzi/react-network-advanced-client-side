3b71b782a72ec3f8f2a62ffc779121d8d98b5467
Juntao Qiu <juntao.qiu@gmail.com>
2024-01-08 19:01:17 +1100

    added nextui for popover

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Profile } from "./v4/profile.tsx";
+import { NextUIProvider } from "@nextui-org/react";
 
 function App() {
   return (
-    <div className="max-w-3xl m-auto my-4 text-slate-800">
-      <h1 className="text-4xl py-4 mb-4 tracking-wider font-bold">Profile</h1>
-      <div className="rounded border p-6 border-slate-300">
-        <Profile id="u1" />
+    <NextUIProvider>
+      <div className="max-w-3xl m-auto my-4 text-slate-800">
+        <h1 className="text-4xl py-4 mb-4 tracking-wider font-bold">Profile</h1>
+        <div className="rounded border p-6 border-slate-300">
+          <Profile id="u1" />
+        </div>
       </div>
-    </div>
+    </NextUIProvider>
   );
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,15 @@
+const {nextui} = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 }
